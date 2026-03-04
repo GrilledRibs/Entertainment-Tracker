6d1852619a04d3a760d7483edc5f1227c65f60a6
GrilledRibs <reevenva@gmail.com>
2026-03-04 16:18:13 +1100

    added delete genre feature and updated dropdown box labels

--- Entertainment_Tracker/obj/Debug/net10.0/Entertainment_Tracker.AssemblyInfo.cs
+++ Entertainment_Tracker/obj/Debug/net10.0/Entertainment_Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entertainment_Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa5d7519623757f43d41d1a7247dc0184e6387f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d583e1495a06b08c0e03efc178266957a5992b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entertainment_Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entertainment_Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
