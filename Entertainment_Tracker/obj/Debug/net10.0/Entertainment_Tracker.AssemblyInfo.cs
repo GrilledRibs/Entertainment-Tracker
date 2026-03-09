dcf85677f6c730ac57290c27355a154e068cdafd
GrilledRibs <reevenva@gmail.com>
2026-03-09 16:03:05 +1100

    added completed Entry page, updated books page to display books in blocks and adjusts dynamically to book count

--- Entertainment_Tracker/obj/Debug/net10.0/Entertainment_Tracker.AssemblyInfo.cs
+++ Entertainment_Tracker/obj/Debug/net10.0/Entertainment_Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entertainment_Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a6577a3a5f574baf6466d89a39bf6b64342194")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6398674900db05787b9b93e72e2d1280946ee77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entertainment_Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entertainment_Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
